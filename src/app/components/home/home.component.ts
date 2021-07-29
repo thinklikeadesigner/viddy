1a024b256ab4c603c6ce2c5ff5806a7813e3e3c1
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-07-29 21:41:34 +0200

    api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
+import { APIResponse, Game } from 'src/app/services/models';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort: string;
-  constructor() {}
+  public games: Array<Game>;
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+      });
+  }
 }
--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -1,4 +1,5 @@
 import {
+  HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
@@ -6,7 +7,32 @@ import {
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable
+// @Injectable()
+// export class HttpHeadersInterceptor implements HttpInterceptor {
+//   constructor() {}
+
+//   intercept(
+//     req: HttpRequest<any>,
+//     next: HttpHandler
+//   ): Observable<HttpEvent<any>> {
+//     req = req.clone({
+//       setHeaders: {
+//         'x-rapidapi-key': '6df9882ad5msh57783a8506f68c4p1a8a98jsn39883793655c',
+//         'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+//       },
+//       setParams: {
+//         key: '207edfe650d54783b683c1bcd207a928',
+//       },
+//     });
+//     return next.handle(req);
+//   }
+// }
+
+// import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+// import { Injectable } from "@angular/core";
+// import { Observable } from "rxjs";
+
+@Injectable()
 export class HttpHeadersInterceptor implements HttpInterceptor {
   constructor() {}
 
@@ -14,8 +40,15 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    req: req.clone({
-      setHeaders: {},
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': '6df9882ad5msh57783a8506f68c4p1a8a98jsn39883793655c',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'a0869b38532c454a8c70e606e4d92b7a',
+      },
     });
+    return next.handle(req);
   }
 }
